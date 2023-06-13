4d2ffbc203801a1d9e7574efd4e111c8a60c0a58
stephane <fs@email.com>
2023-06-13 08:05:31 +0200

    TODO added

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,18 @@ fs.writeFileSync(
   `${__dirname}\\..\\themes\\Adeso-Monodark-color-theme.json`,
   monodark
 );
+
+fs.writeFileSync(
+  `${__dirname}\\..\\themes\\Adeso-Monolight-color-theme.json`,
+  monolight
+);
+
+//TODO: change github account name to "adeso"
+//TODO: change github project and change in package
+//TODO: Change documentation and colors
+//TODO: Change themes in package
+//TODO: Create new logo
+//TODO: Update Readme
+//TODO: Create Wiki
+//TODO: Create Github page
+// TODO: look in documentation where to use publisher and organization in package.json
