7a0171095744a7d4ac8130c49d99f02aeda6d6b1
stephane <fs@email.com>
2023-06-14 19:31:04 +0200

    updated todos

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,6 @@ themesList.forEach((theme) => {
   );
 });
 
-//TODO: Simplify Boolean
-//TODO: Learn how to debug different themes in the same Theme pack
 //TODO: change github account name to "adeso"
 //TODO: change github project and change in package
 //TODO: Change documentation and colors
@@ -17,6 +15,8 @@ themesList.forEach((theme) => {
 //TODO: Update Readme
 //TODO: Create Wiki
 //TODO: Create Github page
-// TODO: look in documentation where to use publisher and organization in package.json
+//TODO: separate inline hint background color. make special color for him.
+//TODO: check if we need to create specific color for list item selected
+//TODO: check color library to work with colors.
 //TODO: check how i establish the backgroud and foreground generic color https://www.youtube.com/watch?v=QCqWzb-9Sy8 10:48
-// Try to make parameters looks good like in the image of https://marketplace.visualstudio.com/items?itemName=dracula-theme.theme-dracula
+//TODO:  Try to make parameters looks good like in the image of https://marketplace.visualstudio.com/items?itemName=dracula-theme.theme-dracula
